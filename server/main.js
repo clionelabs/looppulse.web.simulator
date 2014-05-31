45c7cdc08c9c81a4b43e9caf537381af717f0456
Thomas Pun <thomas@pun.io>
2014-05-31 14:36:59 +0800

    Prepare pending events

--- server/main.js
+++ server/main.js
@@ -1,3 +1,72 @@
+var simulationConfig = Meteor.settings;
 
+var pendingEvents = [];
 
-console.log("setting:" + JSON.stringify(Meteor.settings));
+function addMilliseconds(date, ms) {
+  return new Date(date + ms);
+}
+
+function generateRangeEvent(visitor, path, beaconConfig) {
+    var delay = path.delay;
+    var enteredAt = addMilliseconds(startTime, delay);
+    var pendingEvents = [];
+    return {
+        delay: delay,
+        data: {
+          "type": "didRangeBeacons",
+          "uuid": beaconConfig.uuid,
+          "major": beaconConfig.major,
+          "minor": beaconConfig.minor,
+          "visitor_uuid": visitor.uuid,
+          "created_at": enteredAt
+        }
+    };
+}
+
+function generateRangeEvents(visitor, path, beaconConfig) {
+  var events = [];
+  _(5).times(function (n) {
+      var event = generateRangeEvent(visitor, path, beaconConfig);
+      event.delay += n * 10;
+      events.push(event);
+  });
+  return events;
+}
+
+function generateEvents(visitor, path, beaconConfig) {
+    var events = generateRangeEvents(visitor, path, beaconConfig);
+
+    var delay = path.delay + path.duration;
+    var exitedAt = addMilliseconds(startTime, delay);
+    events.push({
+      delay: delay,
+      data: {
+        "type": "didExitRegion",
+        "uuid": beaconConfig.uuid,
+        "major": beaconConfig.major,
+        "minor": beaconConfig.minor,
+        "visitor_uuid": visitor.uuid,
+        "created_at": exitedAt
+      }
+    });
+
+    return events;
+}
+
+var startTime = Date.now();
+
+_.each(simulationConfig.Visitors, function(visitor, key) {
+    visitor.Paths.forEach(function(path) {
+      var beaconConfig = simulationConfig.Beacons[path.beacon];
+      pendingEvents = pendingEvents.concat(generateEvents(visitor, path, beaconConfig));
+    });
+  }
+);
+
+console.log("count:" + pendingEvents.length);
+
+pendingEvents.forEach(function(event) {
+  setTimeout(function() {
+    console.log("event: " + Date.now() + JSON.stringify(event));
+  }, event.delay);
+});
