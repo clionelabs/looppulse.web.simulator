78bc48b52fbe388c94c41cc5cdfe0f79d56c5ad7
Thomas Pun <thomas@pun.io>
2014-05-31 16:40:03 +0800

    Clean some code

--- server/main.js
+++ server/main.js
@@ -9,19 +9,8 @@ if (simulationConfig.ForceReset) {
 Events.find().observe({
   'added': function(doc) {
     firebase.push(doc);
-    // console.log('adding doc: '+ JSON.stringify(doc));
   }
 });
-
-var pendingEvents = [];
-
-function addMilliseconds(date, ms) {
-  return new Date(date + ms);
-}
-
-function dateToString(date) {
-  return date.toString();
-}
 
 function generateRangeEvent(visitor, path, beaconConfig) {
   var event = new RangeEvent(visitor, path, beaconConfig);
@@ -29,13 +18,11 @@ function generateRangeEvent(visitor, path, beaconConfig) {
 }
 
 function generateRangeEvents(visitor, path, beaconConfig) {
-  var events = [];
   _(5).times(function (n) {
     setTimeout(function() {
       generateRangeEvent(visitor, path, beaconConfig)
     }, n * 1000);
   });
-  return events;
 }
 
 function generateExitEvent(visitor, path, beaconConfig) {
@@ -57,12 +44,3 @@ _.each(simulationConfig.Visitors, function(visitor, key) {
     });
   }
 );
-
-console.log("count: " + pendingEvents.length);
-
-pendingEvents.forEach(function(event) {
-  setTimeout(function() {
-    console.log("event: " + Date.now() + JSON.stringify(event));
-    // firebase.push(event.data);
-  }, event.delay);
-});
